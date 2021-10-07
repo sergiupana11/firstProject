4defc57e7e408e3808e1cc6cd06d412055f200e3
sergi <asd>
2021-10-07 16:55:00 +0300

    test commit 2

--- src/Lab0.java
+++ src/Lab0.java
@@ -1,6 +1,7 @@
 public class Lab0 {
     public static void main(String[] args) {
         System.out.println("AAAA");
-        System.out.println("x");
+        System.out.println("POO RULZ");
+        System.out.println("merge???");
     }
 }
